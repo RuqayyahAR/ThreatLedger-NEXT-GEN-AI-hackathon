3f7a5cdefd15355ab34534b6d5964b94f95f1ebf
Amatul Raheem Ruqayyah <amatul.r.ruqayyah@gmail.com>
2026-07-22 00:36:13 +0530

    Delete ThreatLedger-NEXT-GEN-AI-hackathon-main directory

--- ThreatLedger-NEXT-GEN-AI-hackathon-main/Backend/src/main/java/com/threatledger/backend/repository/NodeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.threatledger.backend.repository;
-
-import com.threatledger.backend.entity.Node;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NodeRepository extends JpaRepository<Node, String> {
-}
